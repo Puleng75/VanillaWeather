19a52fff889a8ec94a24d60e425087e25939a5d7
Puleng75 <133369426+Puleng75@users.noreply.github.com>
2024-02-21 15:13:37 +0200

    added and remove some elements on HTML and fixed errors on js and added some code.

--- index.js
+++ index.js
@@ -3,13 +3,47 @@ function refreshWeather(response) {
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
     cityElement.inneHTML = response.data.city;
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
+
+    cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    temperatureElement.innerHTML = Math.round(temperature);
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
     temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
     let apiKey = "e08ff04e756ab6a30t3eb7172o1c9fa9";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(refreshWeather);
 }
 function handleSearchSubmit(event) {
@@ -20,8 +54,8 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 
 }
-    let searchFormElement = document.querySelector("#search-form");
+    let searchFormElement = document.querySelector(".search-form");
 
     searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-    searchCity("Lisbon");
+    searchCity("Paris");
